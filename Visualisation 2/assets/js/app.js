39d08fae6bd42096436602f75c873a14c06542ff
LianzhengXie <113651877+LianzhengXie@users.noreply.github.com>
2022-10-17 14:33:06 +0800

    Update app.js

--- Visualisation 2/assets/js/app.js
+++ Visualisation 2/assets/js/app.js
@@ -1,14 +1,15 @@
-const vis1 = "assets/data/map.json";
+const vis1 = "https://raw.githubusercontent.com/LianzhengXie/fit3179/main/Visualisation%202/assets/js/map.json";
 
 vegaEmbed('#chart1', vis1).then(function (result) {
 }).catch(console.error);
 
-const vis2 = "assets/data/new_cases.json";
+const vis2 = "https://raw.githubusercontent.com/LianzhengXie/fit3179/main/Visualisation%202/assets/js/new_cases.json";
 
 vegaEmbed('#chart2', vis2).then(function (result) {
 }).catch(console.error);
 
-const vis3 = "assets/data/total_deaths.json";
+const vis3 = "https://raw.githubusercontent.com/LianzhengXie/fit3179/main/Visualisation%202/assets/js/total_deaths.json";
 
 vegaEmbed('#chart3', vis3).then(function (result) {
 }).catch(console.error);
+
